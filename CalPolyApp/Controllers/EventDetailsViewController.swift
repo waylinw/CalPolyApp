68b2d68268469399b1799592ac2370c592b80f10
waylinw <waylinw@yahoo.com>
2017-11-13 15:31:22 -0800

    Setting up messaging screen

--- CalPolyApp/Controllers/EventDetailsViewController.swift
+++ CalPolyApp/Controllers/EventDetailsViewController.swift
@@ -12,11 +12,50 @@ import JSQMessagesViewController
 
 class EventDetailsViewController: JSQMessagesViewController {
     var currentNoteItem: NoteItem?
+    var messages = [JSQMessage]()
+    lazy var outgoingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    }()
+    
+    lazy var incomingBubble: JSQMessagesBubbleImage = {
+        return JSQMessagesBubbleImageFactory()!.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        senderId = "1234"
-        senderDisplayName = "..."
+        senderId = currentNoteItem?.note.userID
+        senderDisplayName = "Me"
+        
+        inputToolbar.contentView.leftBarButtonItem = nil
+        collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
+        collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
     }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        return messages[indexPath.item].senderId == senderId ? outgoingBubble : incomingBubble
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString!
+    {
+        return messages[indexPath.item].senderId == senderId ? nil : NSAttributedString(string: messages[indexPath.item].senderDisplayName)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat
+    {
+        return messages[indexPath.item].senderId == senderId ? 0 : 15
+    }
+
 }
